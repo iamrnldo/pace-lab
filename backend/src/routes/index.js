1e4b53516a59ca0600bbf9471f67eb28de9baf35
iamrnldo <iamrnldo@gmail.com>
2026-08-09 04:53:47 +0700

    Training program fixed

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,6 +4,7 @@ const authRoutes = require("./authRoutes");
 const adminRoutes = require("./adminRoutes");
 const userRoutes = require("./userRoutes");
 const calculatorRoutes = require("./calculatorRoutes");
+const trainingProgramRoutes = require("./trainingProgramRoutes"); // 1. IMPORT DISINI
 
 const router = Router();
 
@@ -11,6 +12,7 @@ router.use("/auth", authRoutes);
 router.use("/admin", adminRoutes);
 router.use("/user", userRoutes);
 router.use("/calculator", calculatorRoutes);
+router.use("/training-program", trainingProgramRoutes); // 2. DAFTARKAN DISINI
 
 // Health check
 router.get("/health", (req, res) => {
